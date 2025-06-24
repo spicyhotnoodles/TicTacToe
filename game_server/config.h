0c5ab2e0be8e2efd5e182c8c09699a560710e6c3
Pietro Ciuci <pietro.ciuci@gmail.com>
2025-06-24 20:40:13 +0200

    Create `config.h`

---
+++ game_server/config.h
@@ -0,0 +1,12 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Game configuration constants
+#define PORT 12345
+#define MAX_MSG_LEN 256
+#define MAX_PLAYERS 5
+#define MAX_GAMES 50
+#define MAX_GAMES_PER_PLAYER 5
+#define PLAYER_TABLE_SIZE 32
+
+#endif
