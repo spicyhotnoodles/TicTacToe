cd67e5fbc60017d5a28d9f577c7cc2d9fdd0e045
Pietro Ciuci <pietro.ciuci@gmail.com>
2025-06-24 20:40:31 +0200

    Create `hash.c`/`h`

---
+++ game_server/hash.c
@@ -0,0 +1,51 @@
+#include "hash.h"
+
+int hash_fd(int fd) {
+    return fd % PLAYER_TABLE_SIZE;
+}
+
+bool player_add(struct player p) {
+    int index = hash_fd(p.fd);
+    for (int i = 0; i < PLAYER_TABLE_SIZE; i++) {
+        int try = (index + i) % PLAYER_TABLE_SIZE;
+        if (!player_table[try].in_use) {
+            player_table[try].fd = p.fd;
+            player_table[try].data = p;
+            player_table[try].in_use = true;
+            return true;
+        }
+    }
+    return false; // Table full
+}
+
+struct player* player_get(int fd) {
+    int index = hash_fd(fd);
+    for (int i = 0; i < PLAYER_TABLE_SIZE; i++) {
+        int try = (index + i) % PLAYER_TABLE_SIZE;
+        if (player_table[try].in_use && player_table[try].fd == fd) {
+            return &player_table[try].data;
+        }
+    }
+    return NULL;
+}
+
+bool player_remove(int fd) {
+    int index = hash_fd(fd);
+    for (int i = 0; i < PLAYER_TABLE_SIZE; i++) {
+        int try = (index + i) % PLAYER_TABLE_SIZE;
+        if (player_table[try].in_use && player_table[try].fd == fd) {
+            player_table[try].in_use = false;
+            return true;
+        }
+    }
+    return false;
+}
+
+bool username_exists(const char* username) {
+    for (int i = 0; i < PLAYER_TABLE_SIZE; i++) {
+        if (player_table[i].in_use && strcmp(player_table[i].data.username, username) == 0) {
+            return true;
+        }
+    }
+    return false;
+}
---
+++ game_server/hash.h
@@ -0,0 +1,21 @@
+#ifndef HASH_H
+#define HASH_H
+
+#include "types.h"
+
+// Hash function to map file descriptor to an index in the player table
+int hash_fd(int fd);
+
+// Function to find a player by file descriptor
+struct player_table_entry *find_player_by_fd(int fd);
+
+// Function to add a new player to the player table
+bool player_add(struct player p);
+
+// Function to remove a player from the player table
+bool player_remove(int fd);
+
+// Function to check if a username already exists in the player table
+bool username_exists(const char* username);
+
+#endif
