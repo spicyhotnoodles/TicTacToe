ce006afc0b6719714db71aff09c67a7f7ef520ba
Pietro Ciuci <46612473+spicyhotnoodles@users.noreply.github.com>
2025-08-28 15:24:49 +0200

    Update communication.h

--- game_server/communication.h
+++ game_server/communication.h
@@ -6,7 +6,6 @@
 #include "json.h"
 #include "game.h"
 
-// Function prototypes for communication handling
 void handle_request(int fd, message_t *message);
 bool send_message(int fd, message_t *message);
 message_t * receive_message(int fd);
