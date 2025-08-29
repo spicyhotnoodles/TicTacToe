cdc9d0aab0d962cfdb2d5ed2114ebf170715ca47
Pietro Ciuci <pietro.ciuci@gmail.com>
2025-08-29 15:06:03 +0200

    Increases server capacity
    Updates configuration to increase server capacity.
    
    Specifically, it increases the maximum message length, number of players, and number of games.
    It also updates the player table size to accommodate more players.

--- game_server/config.h
+++ game_server/config.h
@@ -3,10 +3,10 @@
 
 // Game configuration constants
 #define PORT 12345
-#define MAX_MSG_LEN 256
-#define MAX_PLAYERS 5
-#define MAX_GAMES 50
+#define MAX_MSG_LEN 2049
+#define MAX_PLAYERS 50
+#define MAX_GAMES 250
 #define MAX_GAMES_PER_PLAYER 5
-#define PLAYER_TABLE_SIZE 32
+#define PLAYER_TABLE_SIZE 50
 
 #endif
